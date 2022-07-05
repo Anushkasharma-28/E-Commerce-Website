1d760c657fbea5d9ddb53388c2c022765cb5ad74
Anushkasharma-28 <64864572+Anushkasharma-28@users.noreply.github.com>
2022-07-05 22:33:07 +0530

    Create products.js

---
+++ products.js
@@ -0,0 +1,83 @@
+[
+   { 
+    "name":"Shoes",
+    "price":"2000-10000",
+    "description":"Alpine Running Shoes For Men  (Grey)",
+    "src": "https://rukminim2.flixcart.com/image/800/960/l19m93k0/shoe/h/g/r/10-381478-10-puma-aqua-gray-limepunch-original-imagcv8fktwfnpfz.jpeg?q=50",
+    "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaL"
+    },
+
+ { 
+   "name":"Bags for kids",
+   "price":"1000-4000",
+   "description":"Stylish Girls Cute Backpack For School",
+   "src": "https://rukminim2.flixcart.com/image/800/960/kx25ksw0/backpack/a/d/h/20-blue-panda-6-bpanda-backpack-togrow-sevices-8-20-original-imag9hhzrv5fuk6h.jpeg?q=50",
+   "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaM"
+  },
+
+ { 
+   "name":"Mobile Phones",
+   "price":"5000-40000",
+   "description":"Shop for Android, iOS, Dual-SIM mobiles & More.",
+    "src": "https://www.thechennaimobiles.com/image/cache/catalog/ip13pnk-600x600.jpg",
+    "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaN" 
+  },
+
+ { 
+   "name":"Clothing",
+   "price":"500-1000",
+   "description":"Explore the latest collection of polo t-shirts, crew neck t-shirts, sleeveless t-shirts",
+    "src": "https://cf.shopee.in/file/68ea5a1636338410d7067ed093d632cc" ,
+    "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaO"
+  },
+
+ {
+    "name":"TV Entertainment",
+    "price":"15000-30000",
+    "description":"Perfect Homes Engineered Wood TV Entertainment Unit",
+    "src": "https://rukminim2.flixcart.com/image/416/416/kyoqmq80/tv-entertainment-unit/o/k/s/-original-imagav2rkpmzgqur.jpeg?q=70",
+    "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaP" 
+ 
+  },
+
+ { 
+   "name":"Sports Equipment",
+   "price":"1000-15000",
+   "description":"Explore our collection of sporting gear,gadgets and much more for all sports including Cricket, Badminton, Lawn Tennis, Golf, Basketball, Table Tennis etc",
+   "src": "https://previews.123rf.com/images/lightwise/lightwise1301/lightwise130100068/17472622-sports-equipment.jpg",
+   "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaQ"
+  },
+
+ {
+    "name":"Pet Suplies",
+   "price":"200-2000",
+    "description":"Shop from largest pet supplies store",
+    "src": "https://www.gannett-cdn.com/presto/2020/04/10/USAT/eb927ba8-c0c6-4f11-bd21-43e5781e2540-petsmart.png?width=660&height=372&fit=crop&format=pjpg&auto=webp",
+    "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaR"
+ },
+ { 
+   "name":"Utensils",
+  "price":"500-5000",
+ "description":"Select from the best range of cooking utensils, Tongs, Ladles, Spoons, Spatulas, Whisks, Skimmers etc",
+ "src": "https://m.media-amazon.com/images/I/61bB+hQDQ6L._SY450_.jpg",
+ "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaS" 
+},
+
+ { 
+   "name":"Accesories",
+  "price":"100-3000",
+ "description":" Refresh your look with hats, scarves, belts and more",
+ "src": "https://media.istockphoto.com/photos/top-view-of-female-fashion-accessories-picture-id531786318?k=20&m=531786318&s=612x612&w=0&h=V7bp-KSd-SCn-Ud8JmgSbDc4EL3XHgq91eUEVJDEad4=",
+ "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaU" 
+},
+
+ {
+  "name":"ACs",
+  "price":"25000-60000",
+   "description":"Air Conditioners",
+   "src": "https://cdn.pixabay.com/photo/2021/09/08/07/20/air-conditioner-6605973__340.jpg",
+    "_id":"vyjKrERaTa8URY-z8MB_1Il3EvDFgkaV"
+
+  }
+
+  ]
